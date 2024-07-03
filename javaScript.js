b76abc532e8c3ec02b90e006cb5ea107b2cc1760
nicolas17956 <73597543+nicolas17956@users.noreply.github.com>
2024-07-03 00:36:40 -0300

    scroll

--- javaScript.js
+++ javaScript.js
@@ -2,14 +2,17 @@ document.querySelector(".barra-abrir").addEventListener("click", () => {
     document.querySelector(".nav").style.display = "flex";
     document.querySelector(".barra-abrir").style.display = "none";
     document.querySelector(".barra-cerrar").style.display = "flex";
+    document.querySelector("html").style.overflow = "hidden";
 });
 document.querySelector(".barra-cerrar").addEventListener("click", () => {
     document.querySelector(".nav").style.display = "none";
     document.querySelector(".barra-cerrar").style.display = "none";
     document.querySelector(".barra-abrir").style.display = "flex";
+    document.querySelector("html").style.overflow = "visible";
 });
 document.querySelector(".ul").addEventListener("click", () => {
     document.querySelector(".nav").style.display = "none";
     document.querySelector(".barra-cerrar").style.display = "none";
     document.querySelector(".barra-abrir").style.display = "flex";
+    document.querySelector("html").style.overflow = "visible";
 });
